e1b851f932443cd2e8bd8a40d48aefbd688bf812
Joey Baker <joey@byjoeybaker.com>
2016-10-31 12:41:34 -0700

    Test: add a second mock team
    So that we can test complex sorts

--- src/crud-list-order.integration.test.js
+++ src/crud-list-order.integration.test.js
@@ -9,7 +9,7 @@ setup(test);
 
 test('/players?order=name', async (t) => {
   const { server, instances } = t.context;
-  const { player1, player2 } = instances;
+  const { player1, player2, player3 } = instances;
   const url = '/players?order=name';
   const method = 'GET';
 
@@ -18,11 +18,12 @@ test('/players?order=name', async (t) => {
   // this is the order we'd expect the names to be in
   t.is(result[0].name, player1.name);
   t.is(result[1].name, player2.name);
+  t.is(result[2].name, player3.name);
 });
 
 test('/players?order=name%20ASC', async (t) => {
   const { server, instances } = t.context;
-  const { player1, player2 } = instances;
+  const { player1, player2, player3 } = instances;
   const url = '/players?order=name%20ASC';
   const method = 'GET';
 
@@ -31,24 +32,26 @@ test('/players?order=name%20ASC', async (t) => {
   // this is the order we'd expect the names to be in
   t.is(result[0].name, player1.name);
   t.is(result[1].name, player2.name);
+  t.is(result[2].name, player3.name);
 });
 
 test('/players?order=name%20DESC', async (t) => {
   const { server, instances } = t.context;
-  const { player1, player2 } = instances;
+  const { player1, player2, player3 } = instances;
   const url = '/players?order=name%20DESC';
   const method = 'GET';
 
   const { result, statusCode } = await server.inject({ url, method });
   t.is(statusCode, STATUS_OK);
   // this is the order we'd expect the names to be in
-  t.is(result[0].name, player2.name);
-  t.is(result[1].name, player1.name);
+  t.is(result[0].name, player3.name);
+  t.is(result[1].name, player2.name);
+  t.is(result[2].name, player1.name);
 });
 
 test('/players?order[]=name', async (t) => {
   const { server, instances } = t.context;
-  const { player1, player2 } = instances;
+  const { player1, player2, player3 } = instances;
   const url = '/players?order[]=name';
   const method = 'GET';
 
@@ -57,18 +60,21 @@ test('/players?order[]=name', async (t) => {
   // this is the order we'd expect the names to be in
   t.is(result[0].name, player1.name);
   t.is(result[1].name, player2.name);
+  t.is(result[2].name, player3.name);
 });
 
 test('/players?order[0]=name&order[0]=DESC', async (t) => {
   const { server, instances } = t.context;
-  const { player1, player2 } = instances;
+  const { player1, player2, player3 } = instances;
   const url = '/players?order[0]=name&order[0]=DESC';
   const method = 'GET';
 
   const { result, statusCode } = await server.inject({ url, method });
   t.is(statusCode, STATUS_OK);
   // this is the order we'd expect the names to be in
-  t.is(result[0].name, player2.name);
+  t.is(result[0].name, player3.name);
+  t.is(result[1].name, player2.name);
+  t.is(result[2].name, player1.name);
   t.is(result[1].name, player1.name);
 });
 
